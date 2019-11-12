410caeb4053acf22a17903536ec8279010564bdd
ok19aad <ok19aad@cl-mac-j114-25.herts.ac.uk>
2019-11-12 18:19:53 +0000

    Fixed, everything works Shooter and bullet works fine, add background and find good images next

--- dynamics/ViewController.swift
+++ dynamics/ViewController.swift
@@ -13,31 +13,33 @@ class ViewController: UIViewController, SubviewDelegate {
     var gravity_behavior: UIGravityBehavior!;
     var collision_behavior: UICollisionBehavior!;
     
-    var ball_array: [UIImageView]!;
+    var ball_array: [UIImageView] = [];
     
     func spawnBall(x: CGFloat, y: CGFloat, vx: CGFloat, vy: CGFloat)
     {
-        print("(", x, ", ", y, ")");
         let ball = UIImage(named: "ball.jpg");
         let ball_view = UIImageView(image: ball!);
-        ball_view.frame = CGRect(x: x, y: y, width: 64, height: 64);
-        print(ball!);
-        print(ball_view, "\n");
+        ball_view.frame = CGRect(x: x, y: y, width: 32, height: 32);
         ball_array.append(ball_view);
         view.addSubview(ball_view);
-
-        updateBehaviors();
+        
+        gravity_behavior.addItem(ball_view);
+        collision_behavior.addItem(ball_view);
+        dynamic_item_behavior.addItem(ball_view);
+        
+        dynamic_item_behavior.addLinearVelocity(CGPoint(x: vx * 8.1, y: vy * 9.32 - 25), for: ball_view);
     }
-    
+
     func updateBehaviors(){
         dynamic_item_behavior = UIDynamicItemBehavior(items: ball_array);
-        gravity_behavior = UIGravityBehavior(items: ball_array);
         collision_behavior = UICollisionBehavior(items: ball_array);
+        collision_behavior.translatesReferenceBoundsIntoBoundary = true;
+        gravity_behavior = UIGravityBehavior(items: ball_array);
+        dynamic_animator.addBehavior(gravity_behavior);
         dynamic_animator.addBehavior(dynamic_item_behavior);
         dynamic_animator.addBehavior(collision_behavior);
-        dynamic_animator.addBehavior(gravity_behavior);
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad();
         
@@ -47,15 +49,8 @@ class ViewController: UIViewController, SubviewDelegate {
         UIDevice.current.setValue(value, forKey: "orientation");
         
         dynamic_animator = UIDynamicAnimator(referenceView: self.view);
-        // gravity_behavior = UIGravityBehavior(items: [ball]);
-        dynamic_item_behavior = UIDynamicItemBehavior(items: []);
-        collision_behavior = UICollisionBehavior(items: []);
         
-        // dynamic_item_behavior.addLinearVelocity(CGPoint(x: 175, y: -470), for: ball);
-        collision_behavior.translatesReferenceBoundsIntoBoundary = true;
-//        dynamic_animator.addBehavior(dynamic_item_behavior);
-//        dynamic_animator.addBehavior(collision_behavior);
-//        dynamic_animator.addBehavior(gravity_behavior);
+        updateBehaviors();
     }
 
     override var shouldAutorotate: Bool {

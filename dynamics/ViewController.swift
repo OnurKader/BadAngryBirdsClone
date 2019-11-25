fa39b935b851bac1f0e37de123f1b96327019851
ok19aad <ok19aad@cta-ab270-18.herts.ac.uk>
2019-11-25 14:01:26 +0000

    Added a heron and updated some stuff

--- dynamics/ViewController.swift
+++ dynamics/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, SubviewDelegate {
     var collision_behavior: UICollisionBehavior!;
 
     var screen_size : CGRect!;
+    
+    var ball_array: [UIImageView]! = [];
 
     func spawnBall(x: CGFloat, y: CGFloat, vx: CGFloat, vy: CGFloat)
     {
@@ -21,12 +23,13 @@ class ViewController: UIViewController, SubviewDelegate {
         let ball_view = UIImageView(image: ball!);
         ball_view.frame = CGRect(x: x, y: y, width: 32, height: 32);
         view.addSubview(ball_view);
+        
+        ball_array.append(ball_view);
 
         gravity_behavior.addItem(ball_view);
         collision_behavior.addItem(ball_view);
         dynamic_item_behavior.addItem(ball_view);
-
-        dynamic_item_behavior.addLinearVelocity(CGPoint(x: vx * 15 - 20, y: vy * 10 - 42), for: ball_view);
+        dynamic_item_behavior.addLinearVelocity(CGPoint(x: vx * 15.15 - 20, y: vy * 10 - 50), for: ball_view);
     }
 
     func updateBehaviors(){
@@ -45,18 +48,41 @@ class ViewController: UIViewController, SubviewDelegate {
         dynamic_animator.addBehavior(collision_behavior);
     }
 
+    @objc func update()
+    {
+        for ball in ball_array
+        {
+            // If ball is out of view frame, erase it from the dynamicItemBeh and array.
+            // But not delete the var itself, I don't know how, maybe the thing in the array is a pointer
+            if ((ball.frame.minX > screen_size!.maxX) || (ball.frame.minY > screen_size!.maxY)) {
+                dynamic_item_behavior.removeItem(ball);
+                if let idx = ball_array.firstIndex(of: ball) {
+                    ball_array.remove(at: idx);
+                }
+            }
+        }
+    }
+    
+    func spawnBirds()
+    {
+        
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad();
         screen_size = UIScreen.main.bounds;
-        
+
         shooter.my_delegate = self;
         shooter.center.x = shooter.bounds.midX * 3;
         shooter.center.y = screen_size.height / 2;
-        
+
         let value = UIInterfaceOrientation.landscapeLeft.rawValue;
         UIDevice.current.setValue(value, forKey: "orientation");
 
         updateBehaviors();
+
+        // Schedule update() to be called every second to not be intensive, could drop down to 20ms for 50 fps.
+        _ = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true);
     }
 
     override var shouldAutorotate: Bool {
